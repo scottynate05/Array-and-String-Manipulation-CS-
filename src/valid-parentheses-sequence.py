4ad5514673ee57f66e1fd3c7ee095a7d6a08fdf5
Nathan Brown <scottynate05@yahoo.com>
2020-11-11 21:19:33 -0800

    valid parentheses sequence setup

---
+++ src/valid-parentheses-sequence.py
@@ -0,0 +1,23 @@
+# You are given a parentheses sequence, check if it's regular.
+
+# Example
+
+# For s = "()()(())", the output should be
+# validParenthesesSequence(s) = true;
+# For s = "()()())", the output should be
+# validParenthesesSequence(s) = false.
+# Input/Output
+
+# [execution time limit] 4 seconds (py3)
+
+# [input] string s
+
+# A string, consisting only of '(' and ')'.
+
+# Guaranteed constraints:
+# 0 ≤ s.length ≤ 105.
+
+# [output] boolean
+
+# true is the sequence is regular and false otherwise.
+
