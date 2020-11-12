ecbdc392302f4e53046b865e73f07231550f563c
Nathan Brown <scottynate05@yahoo.com>
2020-11-11 21:23:31 -0800

    valid parentheses sequence complete

--- src/valid-parentheses-sequence.py
+++ src/valid-parentheses-sequence.py
@@ -21,3 +21,13 @@
 
 # true is the sequence is regular and false otherwise.
 
+def validParenthesesSequence(s):
+    count = 0
+    for i in s:
+        if i == '(':
+            count += 1
+        elif i == ')':
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
